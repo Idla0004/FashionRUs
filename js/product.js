7d979dfbf658ae165eab47feb14aa1cabf09e39c
Ida <idla0004@stud.kea.dk>
2025-09-25 15:50:40 +0200

    Opdateret med JS id'er

---
+++ js/product.js
@@ -0,0 +1,49 @@
+const mainContent = document.querySelector("#mainContent");
+fetch("https://kea-alt-del.dk/t7/api/products/1525")
+  .then((res) => res.json())
+  .then((product) => {
+    console.log(product.articletype);
+
+    const params = new URLSearchParams(window.location.search);
+    const id = params.get("id");
+
+    mainContent.innerHTML = ` <nav class="listproduct">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="productlist.html">Brands</a></li>
+        <li><a href="#">Puma</a></li>
+        <p class="description">${product.brandname}</p>
+      </nav>
+
+      <section class="product-section">
+        <img
+          src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp"
+          alt="Puma
+        backpack"
+          class="product-image"
+        />
+        <div class="product-content">
+          <h1 class="product-title">${product.brandname}</h1>
+          <div class="product-info">
+            <p class="product-description">
+             ${product.brandbio}
+            </p>
+            <dl class="info-list">
+              <dt>Type</dt>
+              <dd>${product.articletype}</dd>
+              <dt>Color</dt>
+              <dd>${product.basecolour}</dd>
+              <dt>Inventory number</dt>
+              <dd>${product.relid}</dd>
+            </dl>
+          </div>
+        </div>
+        <section class="buy-card">
+          <h1>Puma backpack</h1>
+          <p class="title">${product.variantname}</p>
+          <p class="product-price">DKK ${product.price}</p>
+          <div class="buy-button">
+            <a href="#">Add to basket</a>
+          </div>
+        </section>
+      </section> `;
+  });
