38ba7c64797d864e7b6e3a004ba649f5e50fad14
Ida <idla0004@stud.kea.dk>
2025-10-02 10:29:30 +0200

    Rettet kode for pris lav til høj

--- js/productlist.js
+++ js/productlist.js
@@ -1,76 +1,78 @@
 const params = new URLSearchParams(window.location.search);
-const category = params.get(`category`);
-const header = (document.querySelector("h2").textContent = category);
-
+const category = params.get("category");
+document.querySelector("h2").textContent = category;
 const productListContainer = document.querySelector(".products");
-
-let allData;
+document.querySelector("#filters").addEventListener("click", showFiltered);
+document.querySelector("#sorting").addEventListener("click", showSorted);
+let allData, currentDataSet;
 
 function showSorted(event) {
   const direction = event.target.dataset.direction;
   if (direction == "lohi") {
-    allData.sort((a, b) => a.price - b.price);
+    currentDataSet.sort((a, b) => a.price - b.price);
   } else {
-    allData.sort((a, b) => b.price - a.price);
+    currentDataSet.sort((a, b) => b.price - a.price);
   }
-  showProduct(allData);
+  showProduct(currentDataSet);
 }
 
-document
-  .querySelectorAll("#filters button")
-  .forEach((knap) => knap.addEventListener("click", showFiltered));
-document.querySelector("#sorting").addEventListener("click", showSorted);
-
 function showFiltered(event) {
-  const gender = this.dataset.gender;
+  const gender = event.target.dataset.gender;
   if (gender == "All") {
-    showProduct(allData);
+    currentDataSet = allData;
   } else {
-    const fraction = allData.filter((product) => product.gender == gender);
-    showProduct(fraction);
+    currentDataSet = allData.filter((product) => product.gender == gender);
   }
+  showProduct(currentDataSet);
 }
 
 fetch(`https://kea-alt-del.dk/t7/api/products?limit=20&category=${category}`)
   .then((res) => res.json())
   .then((data) => {
     allData = data;
-    showProduct(allData);
+    currentDataSet = data;
+    showProduct(currentDataSet);
   });
 
 function showProduct(products) {
   productListContainer.innerHTML = "";
+  let html = "";
   products.forEach((element) => {
-    productListContainer.innerHTML += `
-  <div class="product-card ${element.soldout ? "soldout" : ""} ${
+    html += `
+      <div class="product-card ${element.soldout ? "soldout" : ""} ${
       element.discount ? "sale" : ""
     }">
-    <img
-      src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
-      alt="Reebok sneakers"
-      class="img-card"
-    />
-    <h2>${element.productdisplayname}</h2>
-    <p class="title">${element.category} | ${element.subcategory}</p>
-    <div class="price-container">
-      <p class="price">
-        DKK ${Math.round(element.price)},-
+        <img
+          src="https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp"
+          alt="${element.productdisplayname}"
+          class="img-card"
+        />
+        <h2>${element.productdisplayname}</h2>
+        <p class="title">${element.category} | ${element.subcategory}</p>
+        <div class="price-container">
+          <p class="price">
+            DKK ${Math.round(element.price)},-
+            ${
+              element.discount
+                ? `<br>Now: DKK ${Math.round(
+                    element.price - (element.price * element.discount) / 100
+                  )},-`
+                : ""
+            }
+          </p>
+          ${
+            element.discount
+              ? `<p class="percent">${element.discount}%</p>`
+              : ""
+          }
+        </div>
         ${
-          element.discount
-            ? `<br>Now: DKK ${Math.round(
-                element.price - (element.price * element.discount) / 100
-              )},-`
+          !element.soldout
+            ? `<button class="read"><a href="product.html?id=${element.id}">Læs mere</a></button>`
             : ""
         }
-      </p>
-      ${element.discount ? `<p class="percent">${element.discount}%</p>` : ""}
-    </div>
-    ${
-      !element.soldout
-        ? `<button class="read"><a href="product.html?id=${element.id}">Læs mere</a></button>`
-        : ""
-    }
-  </div>
-`;
+      </div>
+    `;
   });
+  productListContainer.innerHTML = html;
 }
